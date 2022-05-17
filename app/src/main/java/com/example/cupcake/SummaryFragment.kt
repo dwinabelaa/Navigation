7468db0cf104be20b1d4e650a3174de5f0ba6f9e
dwinabelaa <dwinabela0910@gmail.com>
2022-05-17 21:29:19 +0700

    praktikum shared viewmodel

--- app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.cupcake
 
 import android.content.Intent
@@ -26,18 +11,9 @@ import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentSummaryBinding
 import com.example.cupcake.model.OrderViewModel
 
-/**
- * [SummaryFragment] contains a summary of the order details with a button to share the order
- * via another app.
- */
 class SummaryFragment : Fragment() {
-
-    // Binding object instance corresponding to the fragment_summary.xml layout
-    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
-    // when the view hierarchy is attached to the fragment.
     private var binding: FragmentSummaryBinding? = null
 
-    // Use the 'by activityViewModels()' Kotlin property delegate from the fragment-ktx artifact
     private val sharedViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -59,9 +35,6 @@ class SummaryFragment : Fragment() {
         }
     }
 
-    /**
-     * Submit the order by sharing out the order details to another app via an implicit intent.
-     */
     fun sendOrder() {
         // Construct the order summary text with information from the view model
         val numberOfCupcakes = sharedViewModel.quantity.value ?: 0
@@ -87,9 +60,6 @@ class SummaryFragment : Fragment() {
         }
     }
 
-    /**
-     * Cancel the order and start over.
-     */
     fun cancelOrder() {
         // Reset order in view model
         sharedViewModel.resetOrder()
@@ -98,10 +68,6 @@ class SummaryFragment : Fragment() {
         findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
     }
 
-    /**
-     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
-     * is being removed. As a result, clear out the binding object.
-     */
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
